a1e8f949106c72a60078534046767b908e32da54
Monserrathdm <dmedina5341@uta.edu.ec>
2026-03-21 11:09:43 -0500

    main intregrador

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "Katherine_estructuras.h" 
+#include "Alina_listas.h"          
+#include "Rolando_logica.h"        
+#include "Monse_interfaz.h"        
+
+using namespace std;
+
+int main() {
+
+    ListaJuego miLista; 
+
+    mostrarBienvenida(); 
+
+    iniciarPartida(miLista); 
+
+    return 0;
+}
